90e6be5fd940d0d7aff1be2a01a02d84e7556535
James Coggan <james.coggan@mydrivesolutions.com>
2018-02-25 15:40:04 +0000

    Initialized FireBase in the things app

--- worshopapps/mobile/src/main/java/com/jamescoggan/workshopapp/MobileApplication.kt
+++ worshopapps/mobile/src/main/java/com/jamescoggan/workshopapp/MobileApplication.kt
@@ -1,12 +1,14 @@
 package com.jamescoggan.workshopapp
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import timber.log.Timber
 
 class MobileApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        FirebaseApp.initializeApp(this)
         Timber.plant(Timber.DebugTree())
     }
 }
