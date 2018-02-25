3fbd1b14f002ba69fb64aaa898cf91fd12bd2cfe
James Coggan <james.coggan@mydrivesolutions.com>
2018-02-25 15:41:11 +0000

    Added the FireBase LiveData and saving to the things activity

--- worshopapps/things/src/main/java/com/jamescoggan/workshopapp/MainActivity.kt
+++ worshopapps/things/src/main/java/com/jamescoggan/workshopapp/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.jamescoggan.workshopapp
 
+import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.jamescoggan.common.data.HomeInformation
+import com.jamescoggan.common.data.HomeInformationLiveData
+import com.jamescoggan.common.data.HomeInformationStorage
 import com.jamescoggan.workshopapp.actuators.Actuator
 import com.jamescoggan.workshopapp.actuators.Led
 import com.jamescoggan.workshopapp.port.gpioForButton
@@ -22,6 +28,8 @@ class MainActivity : AppCompatActivity() {
     private var led: Actuator<Boolean> = Led(gpioForLED)
     private var tempSensor: Sensor<Int> = TemperatureSensor(i2cForTempSensor, REFRESH_TIME)
     private var switch: Sensor<Boolean> = Switch(gpioForButton)
+    private var homeInformationLiveData: HomeInformationLiveData? = null
+    private var homeInformationStorage: HomeInformationStorage? = null
 
     private val switchListener = object : OnStateChangeListener<Boolean> {
         override fun onStateChanged(state: Boolean) = onSwitch(state)
@@ -30,6 +38,8 @@ class MainActivity : AppCompatActivity() {
     private val tempSensorListener = object : OnStateChangeListener<Int> {
         override fun onStateChanged(state: Int) = onTemp(state)
     }
+
+    private val homeDataObserver = Observer<HomeInformation> { led.setState(it?.light ?: false) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,9 +55,11 @@ class MainActivity : AppCompatActivity() {
 
         switch.setListener(switchListener)
         tempSensor.setListener(tempSensorListener)
+        loginFirebase()
     }
 
     override fun onStop() {
+        homeInformationLiveData?.removeObserver(homeDataObserver)
         led.close()
         switch.close()
         tempSensor.close()
@@ -57,10 +69,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun onSwitch(state: Boolean) {
         Timber.d("Button pressed: $state")
-        led.setState(state)
+        homeInformationStorage?.saveButtonState(state)
+        homeInformationStorage?.saveLightState(state)
     }
 
     private fun onTemp(state: Int) {
         Timber.d("Current Temperature: $state")
+        homeInformationStorage?.saveTemperature(state.toFloat())
+    }
+
+    private fun loginFirebase() {
+        val firebase = FirebaseAuth.getInstance()
+        firebase.signInAnonymously()
+                .addOnSuccessListener { observeData() }
+                .addOnFailureListener { Timber.e("Failed to login $it") }
+    }
+
+    private fun observeData() {
+        Timber.d("Logged in, observing data")
+        val reference = FirebaseDatabase.getInstance().reference.child("home")
+        homeInformationLiveData = HomeInformationLiveData(reference)
+        homeInformationLiveData?.observe(this, homeDataObserver)
+        homeInformationStorage = HomeInformationStorage(reference)
     }
 }
